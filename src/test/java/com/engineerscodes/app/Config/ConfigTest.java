c7c6e57ae70b7779ecd73a1462652f4d18250bb1
engineerscodes <practicalengineers05@gmail.com>
2023-04-08 22:13:10 +0530

    test

--- src/test/java/com/engineerscodes/app/Config/ConfigTest.java
+++ src/test/java/com/engineerscodes/app/Config/ConfigTest.java
@@ -1,2 +1,50 @@
-package com.engineerscodes.app.Config;public class Config {
+package com.engineerscodes.app.Config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ConfigTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
+    }
+
+    @Test
+    @WithMockUser(username="admin",authorities = {"ks_admin"})
+    public void TestUserWithRoleAdmin() throws Exception {
+        this.mockMvc.perform(get("/all"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(username="admin",authorities = {"ks_user"})
+    public void TestUserWithRoleUser() throws Exception {
+        this.mockMvc.perform(get("/all"))
+                .andExpect(status().isOk());
+    }
+
 }
--- src/test/java/com/engineerscodes/app/KhonshuApplicationTests.java
+++ src/test/java/com/engineerscodes/app/KhonshuApplicationTests.java
@@ -1,13 +1,10 @@
 package com.engineerscodes.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KhonshuApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
